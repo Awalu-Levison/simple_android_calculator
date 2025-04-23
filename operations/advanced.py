d06617ef26b85fb62b7724960a9b2f62b4e709c4
Awalu-Levison <levisonawalu251@gmail.com>
2025-04-23 07:04:56 +0200

    Added logarithm functions as part of advanced fatures

--- operations/advanced.py
+++ operations/advanced.py
@@ -20,6 +20,7 @@ def square_root(value):
         return "Error"
 
 
+# Find percentage of a number
 def percentage(value):
     try:
         num = float(value)
@@ -35,3 +36,38 @@ def exponentiate(base, exponent):
         return str(result)
     except ValueError:
         return "Error"
+
+
+# Logarith function
+def log_base(value, base):
+    try:
+        value = float(value)
+        base = float(base)
+        if value <= 0 or base <= 0:
+            return "Error: log undefined for non-positive numbers"
+        return str(math.log(value, base))
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+# Find log 10
+def log10(value):
+    try:
+        value = float(value)
+        if value <= 0:
+            return "Error: log10 undefined for non-positive numbers"
+        return str(math.log10(value))
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+# ln(x) Natural logarithm
+def ln(value):
+    try:
+        value = float(value)
+        if value <= 0:
+            return "Error: ln undefined for non-positive numbers"
+        return str(math.log(value))
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
